f352c1168fdcb39b6dc274b27c7fe5951226d41f
Kenny <kenhufford@gmail.com>
2019-11-01 16:33:18 -0700

    Finish DFS BFS

--- W3D5/polytreenode/skeleton/lib/00_tree_node.rb
+++ W3D5/polytreenode/skeleton/lib/00_tree_node.rb
@@ -1,5 +1,67 @@
+require "byebug"
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)  #nil
+        @parent.children.keep_if { |ele| ele != self } if @parent != nil
+
+        if node == nil
+            @parent = nil
+        else
+            @parent = node 
+            if !node.children.include?(self)
+                node.children << self
+            end
+        end
+    end
+
+    def add_child(node)
+        node.parent = self
+    end
+
+    def remove_child(node)
+        raise "Error" if !self.children.include?(node)
+        node.parent = nil 
+    end
+    
+    def dfs(target)
+        return self if self.value == target
+        
+        self.children.each do |child| 
+            result = child.dfs(target)
+            return result if result != nil
+        end
+
+        return nil if self.parent == nil
+    end
+
+    def bfs(target)
+    
+        queue = [self]
+        
+        until queue.empty?
+            compare = queue.shift
+            return compare if compare.value == target
+            queue += compare.children
+        end
+        return nil
+    end
+
+end
 
     
 
-end
+# Create a queue with root
+# Until queue.empty?
+# Dequeue node
+# Return node if == target
+# Enqueue node’s children
+# Return nil
+
